77efe951e34ab9cd7a8c6fbc67e67329b699fb24
Tae Hee Jang <kancth03@gmail.com>
2025-01-31 00:54:30 +0900

    feat: Review Repository 구현

---
+++ src/main/kotlin/com/dugaza/letsdrive/repository/ReviewRepository.kt
@@ -0,0 +1,8 @@
+package com.dugaza.letsdrive.repository
+
+import com.dugaza.letsdrive.entity.common.Review
+import org.springframework.data.jpa.repository.JpaRepository
+import java.util.UUID
+
+interface ReviewRepository : JpaRepository<Review, UUID> {
+}
