2cc99222878ae6afb9ba24ad9ea3587c630d6024
Tae Hee Jang <kancth03@gmail.com>
2025-01-31 01:06:19 +0900

    feat: Create Review Result Entity

---
+++ src/main/kotlin/com/dugaza/letsdrive/entity/common/evaluation/EvaluationResult.kt
@@ -0,0 +1,26 @@
+package com.dugaza.letsdrive.entity.common.evaluation
+
+import com.dugaza.letsdrive.entity.base.BaseEntity
+import com.dugaza.letsdrive.entity.common.Review
+import com.dugaza.letsdrive.entity.user.User
+import jakarta.persistence.Entity
+import jakarta.persistence.Table
+import jakarta.persistence.ManyToOne
+import jakarta.persistence.FetchType
+import jakarta.persistence.JoinColumn
+
+@Entity
+@Table(name = "common_evaluation_result")
+class EvaluationResult(
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "review_id", nullable = false)
+    val review: Review,
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "answer_id", nullable = false)
+    val answer: EvaluationAnswer,
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    val user: User,
+    ) : BaseEntity()
