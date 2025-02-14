a005771d08ba9c7cecb4e49665a1a1477e1ccf19
Tae Hee Jang <kancth03@gmail.com>
2025-02-14 19:58:24 +0900

    feat: Review Response DTO가 평가 결과도 반환하도록 변경

--- src/main/kotlin/com/dugaza/letsdrive/dto/review/ReviewResponse.kt
+++ src/main/kotlin/com/dugaza/letsdrive/dto/review/ReviewResponse.kt
@@ -1,25 +1,35 @@
 package com.dugaza.letsdrive.dto.review
 
+import com.dugaza.letsdrive.dto.evaluation.EvaluationResultResponse
 import com.dugaza.letsdrive.entity.common.Review
+import com.dugaza.letsdrive.entity.common.evaluation.EvaluationResult
 import java.util.UUID
 
-data class ReviewResponse(
+class ReviewResponse(
     val reviewId: UUID,
     val userId: UUID,
+    val evaluationResultList: List<EvaluationResultResponse>,
     val score: Double,
     val content: String,
     val isDisplayed: Boolean,
 ) {
     companion object {
         fun of(
-            review: Review
-        ) : ReviewResponse {
+            review: Review,
+            evaluationResultList: List<EvaluationResult>,
+        ): ReviewResponse {
             return ReviewResponse(
                 reviewId = review.id!!,
                 userId = review.user.id!!,
+                evaluationResultList = evaluationResultList.map {
+                    EvaluationResultResponse.of(
+                        question = it.answer.question,
+                        answer = it.answer,
+                    )
+                },
                 score = review.score,
                 content = review.content,
-                isDisplayed = review.isDisplayed
+                isDisplayed = review.isDisplayed,
             )
         }
     }
