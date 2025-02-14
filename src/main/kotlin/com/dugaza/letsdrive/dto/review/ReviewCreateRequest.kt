b2ff4a45137ca87bdde780caa8665a0149f53318
Tae Hee Jang <kancth03@gmail.com>
2025-02-14 19:54:56 +0900

    fix: (ReviewCreateRequest) file 대신 FileMaster ID를 받도록 DTO 변경

--- src/main/kotlin/com/dugaza/letsdrive/dto/review/ReviewCreateRequest.kt
+++ src/main/kotlin/com/dugaza/letsdrive/dto/review/ReviewCreateRequest.kt
@@ -1,22 +1,20 @@
 package com.dugaza.letsdrive.dto.review
 
-import com.dugaza.letsdrive.dto.evaluation.EvaluationResultRequest
 import com.dugaza.letsdrive.service.TargetType
 import com.dugaza.letsdrive.validator.ValidEnum
 import jakarta.validation.constraints.DecimalMax
 import jakarta.validation.constraints.DecimalMin
 import jakarta.validation.constraints.NotBlank
 import org.hibernate.validator.constraints.Length
-import org.springframework.web.multipart.MultipartFile
 import java.util.UUID
 
-class ReviewCreateDto(
+class ReviewCreateRequest(
     @field:NotBlank(message = "targetId는 필수 입력값입니다.")
     val targetId: UUID,
     @field:NotBlank(message = "userId는 필수 입력값입니다.")
     val userId: UUID,
-    @field:NotBlank(message = "evaluationType은 필수 입력값입니다.")
-    val evaluationType: String,
+    @field:NotBlank(message = "evaluationId은 필수 입력값입니다.")
+    val evaluationId: UUID,
     @field:ValidEnum(
         enumClass = TargetType::class,
         message = "Types are only available for Course.",
@@ -25,8 +23,9 @@ class ReviewCreateDto(
     )
     val targetType: TargetType,
     @field:NotBlank(message = "evaluationResultList는 필수 입력값입니다.")
-    val evaluationResultList: List<EvaluationResultRequest>,
-    var files: List<MultipartFile>? = null,
+    val evaluationResultList: List<UUID>,
+    @field:NotBlank(message = "fileMasterId는 필수 입력값입니다.")
+    var fileMasterId: UUID,
     @field:NotBlank(message = "score는 필수 입력값입니다.")
     @field:DecimalMin(
         value = "0.5",
@@ -34,7 +33,7 @@ class ReviewCreateDto(
     )
     @field:DecimalMax(
         value = "5.0",
-        message = "score는 최대 5.0를 초과할 수 없습니다."
+        message = "score는 최대 5.0을 초과할 수 없습니다."
     )
     val score: Double,
     @field:NotBlank(message = "content는 필수 입력값입니다.")
@@ -44,5 +43,4 @@ class ReviewCreateDto(
         message = "content는 1자 이상 4000자 이하로 작성해주세요."
     )
     val content: String,
-) {
-}
+)
