9195dad7be5d9d5fc22713f3c8db4303e4e43911
Tae Hee Jang <kancth03@gmail.com>
2025-03-01 23:29:45 +0900

    fix: review delete api
    requestParam 대신 pathVariable을 사용해 restfull하게 변경

--- src/main/kotlin/com/dugaza/letsdrive/controller/ReviewController.kt
+++ src/main/kotlin/com/dugaza/letsdrive/controller/ReviewController.kt
@@ -15,10 +15,10 @@ import org.springframework.security.core.annotation.AuthenticationPrincipal
 import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PatchMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 import java.util.UUID
 
@@ -86,9 +86,9 @@ class ReviewController(
         )
     }
 
-    @DeleteMapping
+    @DeleteMapping("/{reviewId}")
     fun deleteReview(
-        @RequestParam reviewId: UUID,
+        @PathVariable reviewId: UUID,
         @AuthenticationPrincipal user: CustomOAuth2User,
     ): ResponseEntity<Unit> {
         reviewService.deleteReview(
