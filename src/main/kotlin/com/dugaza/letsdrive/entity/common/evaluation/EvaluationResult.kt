e417028da503118e1ce11e233cef004e4df9c5de
Tae Hee Jang <kancth03@gmail.com>
2025-03-02 00:50:43 +0900

    feat: EvaluationResult Entity SQLDelete 추가

--- src/main/kotlin/com/dugaza/letsdrive/entity/common/evaluation/EvaluationResult.kt
+++ src/main/kotlin/com/dugaza/letsdrive/entity/common/evaluation/EvaluationResult.kt
@@ -8,9 +8,11 @@ import jakarta.persistence.FetchType
 import jakarta.persistence.JoinColumn
 import jakarta.persistence.ManyToOne
 import jakarta.persistence.Table
+import org.hibernate.annotations.SQLDelete
 
 @Entity
 @Table(name = "common_evaluation_result")
+@SQLDelete(sql = "UPDATE common_evaluation_result SET deleted_at = NOW() WHERE id = ?")
 class EvaluationResult(
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "review_id", nullable = false)
