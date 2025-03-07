d029296f1504bfa5cafff8c76c25020d5ce6e352
Tae Hee Jang <kancth03@gmail.com>
2025-03-07 22:58:51 +0900

    refactor: @Repository annoation delete

--- src/main/kotlin/com/dugaza/letsdrive/repository/evaluation/EvaluationAnswerRepository.kt
+++ src/main/kotlin/com/dugaza/letsdrive/repository/evaluation/EvaluationAnswerRepository.kt
@@ -3,10 +3,8 @@ package com.dugaza.letsdrive.repository.evaluation
 import com.dugaza.letsdrive.entity.common.evaluation.EvaluationAnswer
 import com.dugaza.letsdrive.entity.common.evaluation.EvaluationQuestion
 import org.springframework.data.jpa.repository.JpaRepository
-import org.springframework.stereotype.Repository
 import java.util.UUID
 
-@Repository
 interface EvaluationAnswerRepository : JpaRepository<EvaluationAnswer, UUID>, EvaluationAnswerCustomRepository {
     fun existsByQuestionAndAnswer(
         question: EvaluationQuestion,
--- src/main/kotlin/com/dugaza/letsdrive/repository/evaluation/EvaluationResultRepository.kt
+++ src/main/kotlin/com/dugaza/letsdrive/repository/evaluation/EvaluationResultRepository.kt
@@ -2,8 +2,6 @@ package com.dugaza.letsdrive.repository.evaluation
 
 import com.dugaza.letsdrive.entity.common.evaluation.EvaluationResult
 import org.springframework.data.jpa.repository.JpaRepository
-import org.springframework.stereotype.Repository
 import java.util.UUID
 
-@Repository
 interface EvaluationResultRepository : JpaRepository<EvaluationResult, UUID>, EvaluationResultCustomRepository
