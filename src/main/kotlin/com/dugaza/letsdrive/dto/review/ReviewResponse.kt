6cc3ae49144d08015edae7f73d62c3e6d1a69848
Tae Hee Jang <kancth03@gmail.com>
2025-03-22 22:47:52 +0900

    fix: Review 조회 시 작성 User의 데이터를 같이 조회

--- src/main/kotlin/com/dugaza/letsdrive/dto/review/ReviewResponse.kt
+++ src/main/kotlin/com/dugaza/letsdrive/dto/review/ReviewResponse.kt
@@ -3,34 +3,36 @@ package com.dugaza.letsdrive.dto.review
 import com.dugaza.letsdrive.dto.evaluation.EvaluationResultResponse
 import com.dugaza.letsdrive.entity.common.Review
 import com.dugaza.letsdrive.entity.common.evaluation.EvaluationResult
+import com.dugaza.letsdrive.entity.file.FileMaster
+import java.time.LocalDateTime
 import java.util.UUID
 
 class ReviewResponse(
     val reviewId: UUID,
     val userId: UUID,
+    val profileImageId: UUID,
+    val nickname: String,
     val evaluationResultList: List<EvaluationResultResponse>,
     val score: Double,
     val content: String,
-    val isDisplayed: Boolean,
+    val createdAt: LocalDateTime,
 ) {
     companion object {
         fun of(
             review: Review,
-            evaluationResultList: List<EvaluationResult>,
+            evaluationResultList: List<EvaluationResultResponse>,
+            profileImageId: UUID,
+            nickname: String,
         ): ReviewResponse {
             return ReviewResponse(
                 reviewId = review.id!!,
                 userId = review.user.id!!,
-                evaluationResultList =
-                    evaluationResultList.map {
-                        EvaluationResultResponse.of(
-                            question = it.answer.question,
-                            answer = it.answer,
-                        )
-                    },
+                profileImageId = profileImageId,
+                nickname = nickname,
+                evaluationResultList = evaluationResultList,
                 score = review.score,
                 content = review.content,
-                isDisplayed = review.isDisplayed,
+                createdAt = review.createdAt!!,
             )
         }
     }
