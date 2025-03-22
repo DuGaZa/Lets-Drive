fae1c845b4e94049118c1e5196e50368207b8ec8
Tae Hee Jang <kancth03@gmail.com>
2025-03-22 22:50:43 +0900

    feat: review find api 반환 타입 변경

--- src/main/kotlin/com/dugaza/letsdrive/controller/ReviewController.kt
+++ src/main/kotlin/com/dugaza/letsdrive/controller/ReviewController.kt
@@ -1,5 +1,6 @@
 package com.dugaza.letsdrive.controller
 
+import com.dugaza.letsdrive.dto.evaluation.EvaluationResultResponse
 import com.dugaza.letsdrive.dto.review.ModifyReviewRequest
 import com.dugaza.letsdrive.dto.review.ReviewCreateRequest
 import com.dugaza.letsdrive.dto.review.ReviewResponse
@@ -9,10 +10,10 @@ import com.dugaza.letsdrive.service.TargetType
 import com.dugaza.letsdrive.service.evaluation.EvaluationService
 import com.dugaza.letsdrive.service.review.ReviewService
 import jakarta.validation.Valid
-import org.springframework.data.domain.Page
 import org.springframework.data.domain.Pageable
 import org.springframework.data.domain.Sort
 import org.springframework.data.web.PageableDefault
+import org.springframework.data.web.PagedModel
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.security.core.annotation.AuthenticationPrincipal
@@ -60,11 +61,22 @@ class ReviewController(
             .body(
                 ReviewResponse.of(
                     review = createdReview,
-                    evaluationResultList = resultList,
+                    evaluationResultList = resultList.map {
+                        EvaluationResultResponse.of(
+                            it.answer.question,
+                            it.answer,
+                        )
+                    },
+                    profileImageId = createdReview.user.profileImage?.id!!,
+                    nickname = createdReview.user.nickname,
                 ),
             )
     }
 
+    // MEMO: Spring Boot 3.3 버전 이상부터 Page<> 객체가 도메인 객체로 변경되어
+    // 그냥 Page로 JSON 직렬화 시 경고 문구 출력,
+    // PagedModel(Spring Boot에서 구현된 Page DTO)로 반환하도록 변경
+    // [some link](https://velog.io/@solst_ice/3.3-%EC%9D%B4%EC%83%81-%EC%8A%A4%ED%94%84%EB%A7%81-%EB%B6%80%ED%8A%B8%EC%9D%98-%EC%BB%A8%ED%8A%B8%EB%A1%A4%EB%9F%AC%EC%97%90%EC%84%9C-%EC%97%90%EC%84%9C-Page-%EA%B0%9D%EC%B2%B4%EB%A5%BC-%EA%B7%B8%EB%8C%80%EB%A1%9C-%EC%9D%91%EB%8B%B5%ED%95%A0-%EB%95%8C-%EB%B0%9C%EC%83%9D%ED%95%9C-%EA%B2%BD%EA%B3%A0)
     @GetMapping
     fun getReviewList(
         @RequestParam("targetId")
@@ -74,23 +86,17 @@ class ReviewController(
         @PageableDefault(
             size = 10,
             page = 0,
-            sort = ["createdAt"],
+            sort = ["createdAt,desc"],
             direction = Sort.Direction.DESC,
         )
         pageable: Pageable,
-    ): ResponseEntity<Page<ReviewResponse>> {
+    ): ResponseEntity<PagedModel<ReviewResponse>> {
         return ResponseEntity.ok(
             reviewService.getReviewList(
                 targetId = targetId,
                 targetType = targetType,
-            ).map {
-                val resultList =
-                    evaluationService.getEvaluationResultListByReviewId(
-                        userId = it.user.id!!,
-                        reviewId = it.id!!,
-                    )
-                ReviewResponse.of(it, resultList)
-            },
+                pageable = pageable,
+            )
         )
     }
 
@@ -115,7 +121,14 @@ class ReviewController(
         return ResponseEntity.ok(
             ReviewResponse.of(
                 review = modifiedReview,
-                evaluationResultList = resultList,
+                evaluationResultList = resultList.map {
+                    EvaluationResultResponse.of(
+                        it.answer.question,
+                        it.answer,
+                    )
+                },
+                profileImageId = modifiedReview.user.profileImage?.id!!,
+                nickname = modifiedReview.user.nickname,
             ),
         )
     }
